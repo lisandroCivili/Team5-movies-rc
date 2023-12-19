bbbdc1be50915226ec3c4df71f15103c0e424a98
Figueroa Fernando <feroa88@gmail.com>
2023-12-18 21:22:35 -0300

    acomodando los archivos

--- js/appLogin.js
+++ js/appLogin.js
@@ -1,27 +1,72 @@
-const criteriosValue = {
+const validar = {
   // permite mayusculas, minusculas y guiones medio e inferior
 	usuario: {
     exprecion:/^[a-zA-Z0-9áéíóúÁÉÍÓÚüÜñÑ]{6,20}$/,
-    error:'usuario no valido, minimo de caracteres 6 y maximo 20'
+    error:'usuario no valido, minimo de caracteres 6 y maximo 20',
+    requerido: true
   },   
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+={}\[\]:;<>,.?/~])(?!.*\s).{8,20}$/, // 4 a 12 digitos.
+	password:{
+    exprecion: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+={}\[\]:;<>,.?/~])(?!.*\s).{8,20}$/,
+    error:'al menos 1 mayuscula,1 numero,1 carter especial y min 8 max 20 caracteres',
+    requerido:true
+  },  
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+  loadleyendaError: function (input){    
+    const div = document.createElement('div');
+    div.classList.add( 'invalid-feedback');
+    console.log(this[input.name].error);
+    div.textContent = this[input.name].error;
+    input.insertAdjacentElement('afterend',div)
+  },
+  comprobarForm: function (form){
+    let inputs = form.querySelectorAll('input:not([type="checkbox"])');
+    return Array.from(inputs).every(input =>{
+      if (validar[input.name].requerido){
+        return input.classList.contains('is-valid');
+      }else{
+        return input.value ? input.classList.contains('is-valid'): true;
+      }
+    });
+  }
 };
-const formulario = document.forms.frmLogin;
-const inputs = document.querySelectorAll('#frmLogin input');
-console.log(inputs);
+
 let validarForm = (e)=>{
- let tipo= e.target.name
-  console.log(criteriosValue[tipo].exprecion.test(e.target.value))
+  let tipo = e.target.name
+  let input = e.target;
+  let form = input.form;
+  if (input.value){
+    if(!validar[tipo].exprecion.test(input.value)){
+      input.classList.add('is-invalid')
+      input.classList.remove('is-valid')
+    }else{
+      input.classList.remove('is-invalid')
+      input.classList.add('is-valid')
+    }
+  }
+  const boton = form.querySelector('button[type="submit"]');
+  if (validar.comprobarForm(form)){
+    boton.disabled=false;
+  }else{
+    boton.disabled=true;
+  };
+
+  
 }
 
-inputs.forEach(input =>{
+const formLogin = document.forms.frmLogin;
+const inputsLogin = formLogin.querySelectorAll('input:not([type="checkbox"])');
 
+inputsLogin.forEach(input =>{
+  validar.loadleyendaError(input);
   input.addEventListener('keyup',validarForm);
   input.addEventListener('blur',validarForm);
 })
-formulario.addEventListener('submit',(e)=>{
+
+formLogin.addEventListener('submit',(e)=>{
   e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('usuarioNuevoKey')));
+  
+  
 });
