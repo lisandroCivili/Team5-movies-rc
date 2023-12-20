c16193cef89c5cdf1013b2d4c804f70632a54a8a
Lisandro <96018788+lisandroCivili@users.noreply.github.com>
2023-12-20 15:08:45 -0300

    Revert "Enviar mail nuevo"

--- js/contacto.js
+++ js/contacto.js
@@ -1,42 +1,15 @@
 (() => {
-  'use strict';
-  const forms = document.querySelectorAll('.needs-validation');
-
-  Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-          if (!form.checkValidity()) {
-              event.preventDefault();
-              event.stopPropagation();
-              form.classList.add('was-validated');
-          } else {
-              event.preventDefault();
-              sendEmail(form);
-          }
-      }, false);
-  });
-
-  function sendEmail(form) {
-      const btn = form.querySelector('button[type="submit"]');
-      btn.value = 'Sending...';
-
-      const serviceID = 'default_service';
-      const templateID = 'template_ba5dtzf';
-
-      emailjs.sendForm(serviceID, templateID, form)
-          .then(() => {
-              btn.value = 'Send Email';
-              Swal.fire({
-                  title: "Consulta enviada",
-                  text: "Su consulta fue enviada exitosamente!",
-                  icon: "success"
-              });
-          })
-          .catch((err) => {
-              btn.value = 'Send Email';
-              alert(JSON.stringify(err));
-          })
-          .finally(() => {
-              btn.disabled = false;
-          });
-  }
-})();
+    'use strict'
+const forms = document.querySelectorAll('.needs-validation')
+    
+    Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+    }
+    
+    form.classList.add('was-validated')
+    }, false)
+    })
+    })()
